94b7fa09a4e0a4bdf8f9b2a64c47560616b1e97f
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2022-12-13 12:11:28 +0100

    Test

--- CarefulBitesAPI/Controllers/CarefulBitesController.cs
+++ CarefulBitesAPI/Controllers/CarefulBitesController.cs
@@ -77,6 +77,7 @@ namespace CarefulBitesAPI.Controllers {
                 default:
                     return BadRequest();
             }
+
         }
 
         [HttpDelete("foodItems/{itemId}", Name = "DeleteFoodItem")]
